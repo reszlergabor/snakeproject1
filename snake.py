529a269b65d7a0240eee9e6a0ab9e4d4239c4e5e
reszlergabor <reszlerg@gmail.com>
2024-02-17 14:13:26 +0100

    javitasok, fejlesztesek

--- snake.py
+++ snake.py
@@ -3,8 +3,9 @@ from pygame.math import Vector2
 
 class SNAKE:
     def __init__(self):
-        self.body = [Vector2(5,10),Vector2(6,10),Vector2(7,10)]
+        self.body = [Vector2(5,10),Vector2(4,10),Vector2(3,10)]
         self.direction = Vector2(1,0)
+        self.new_block = False
 
     def draw_snake(self):
         for block in self.body:
@@ -16,9 +17,18 @@ class SNAKE:
             pygame.draw.rect(screen,(183,111,122),block_rect)
 
     def move_snake(self):
-        body_copy = self.body[:-1]
-        body_copy.insert(0,body_copy[0] + self.direction)
-        self.body = body_copy[:]
+        if self.new_block == True:
+            body_copy = self.body[:]
+            body_copy.insert(0,body_copy[0] + self.direction)
+            self.body = body_copy[:]
+            self.new_block = False
+        else:
+            body_copy = self.body[:-1]
+            body_copy.insert(0,body_copy[0] + self.direction)
+            self.body = body_copy[:]
+
+    def add_block(self):
+        self.new_block = True     
 
 
 class FRUIT:
@@ -46,6 +56,7 @@ class MAIN:
     def update(self):
         self.snake.move_snake()
         self.check_collision()
+        self.check_fail()
 
     def draw_elements(self):
         self.fruit.draw_fruit()
@@ -56,6 +67,24 @@ class MAIN:
             #reposition the fruit
             self.fruit.randomize()
             #add another block to the snake
+            self.snake.add_block()
+
+    def check_fail(self):
+        #check if snake is outside of the screen
+        if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
+            self.game_over()
+
+        #check snake beleharap-e magaba
+        for block in self.snake.body[1:]:
+            if block == self.snake.body[0]:
+                self.game_over()
+        
+            
+    def game_over(self):
+        pygame.quit()
+        sys.exit()
+
+    
 
 
 
